f1eaad59081654909f0c3f7ec62d0417e8863ff9
ehsan1814 <mr.ehsan.kaji@gmail.com>
2022-04-11 17:52:31 +0430

    add link to dashbutton

--- src/components/App/Dashboard/Dashbar/index.tsx
+++ src/components/App/Dashboard/Dashbar/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
+import Link from 'next/link'
 import Image from 'next/image'
+import styled from 'styled-components'
 
 import { RowCenter } from 'components/Row'
-import BRIDGE_ICON_URL from 'assets/img/dashboard/bridge.svg'
-import FARMS_ICON_URL from 'assets/img/dashboard/farms.svg'
-import FRONTENDS_ICON_URL from 'assets/img/dashboard/frontends.svg'
-import MIGRATOR_ICON_URL from 'assets/img/dashboard/migrator.svg'
-import STABLECOIN_ICON_URL from 'assets/img/dashboard/stablecoin.svg'
+import { ExternalLink } from 'components/Link'
 import SWAP_ICON_URL from 'assets/img/dashboard/swap.svg'
+import FARMS_ICON_URL from 'assets/img/dashboard/farms.svg'
+import BRIDGE_ICON_URL from 'assets/img/dashboard/bridge.svg'
+import MIGRATOR_ICON_URL from 'assets/img/dashboard/migrator.svg'
+import FRONTENDS_ICON_URL from 'assets/img/dashboard/frontends.svg'
+import STABLECOIN_ICON_URL from 'assets/img/dashboard/stablecoin.svg'
 
 const DashboardButtons = styled(RowCenter)`
   flex-wrap: wrap;
@@ -40,24 +42,44 @@ const Label = styled.div`
   font-size: 20px;
 `
 
+const InternalLink = styled.a`
+  text-decoration: none;
+`
+
 const buttons = [
-  { img: STABLECOIN_ICON_URL, label: 'Stablecoin' },
-  { img: BRIDGE_ICON_URL, label: 'Bridge' },
-  { img: FRONTENDS_ICON_URL, label: 'Frontends' },
-  { img: MIGRATOR_ICON_URL, label: 'Migrator' },
-  { img: SWAP_ICON_URL, label: 'Swap' },
-  { img: FARMS_ICON_URL, label: 'Farms' },
+  { img: STABLECOIN_ICON_URL, label: 'Stablecoin', link: '/stablecoin' },
+  { img: BRIDGE_ICON_URL, label: 'Bridge', link: '/bridge' },
+  // TODO: Add frontend & migrator links
+  { img: FRONTENDS_ICON_URL, label: 'Frontends', link: '' },
+  { img: MIGRATOR_ICON_URL, label: 'Migrator', link: '' },
+  { img: SWAP_ICON_URL, label: 'Swap', link: 'https://www.solidly.vision/swap' },
+  { img: FARMS_ICON_URL, label: 'Farms', link: 'https://app.deus.finance/stable/farms' },
 ]
 
-function DashButton({ icon, label }: { icon: string; label: string }) {
+function DashButton({ icon, label, link }: { icon: string; label: string; link: string }) {
   return (
     <ButtonWrap>
-      <StyledButton>
-        <>
-          <Image src={icon} alt={`${label} logo`} />
-          <Label>{label}</Label>
-        </>
-      </StyledButton>
+      {link.charAt(0) === '/' ? (
+        <Link href={link} passHref>
+          <InternalLink>
+            <StyledButton>
+              <>
+                <Image src={icon} alt={`${label} logo`} />
+                <Label>{label}</Label>
+              </>
+            </StyledButton>
+          </InternalLink>
+        </Link>
+      ) : (
+        <ExternalLink href={link} style={{ textDecoration: 'none' }}>
+          <StyledButton>
+            <>
+              <Image src={icon} alt={`${label} logo`} />
+              <Label>{label}</Label>
+            </>
+          </StyledButton>
+        </ExternalLink>
+      )}
     </ButtonWrap>
   )
 }
@@ -66,7 +88,7 @@ export default function Dashbar() {
   return (
     <DashboardButtons>
       {buttons.map((button, index) => (
-        <DashButton key={index} icon={button.img} label={button.label} />
+        <DashButton key={index} icon={button.img} label={button.label} link={button.link} />
       ))}
     </DashboardButtons>
   )
