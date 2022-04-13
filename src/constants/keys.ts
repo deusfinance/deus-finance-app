66f2566a03422005d04fcd096bd439603c927b0c
Mohammad Ali <maliabr75@gmail.com>
2022-04-13 14:46:10 +0430

    feat: add keys

--- src/constants/keys.ts
+++ src/constants/keys.ts
@@ -1,6 +1,7 @@
 export const INFURA_KEY = process.env.NEXT_PUBLIC_INFURA_KEY
 export const FORMATIC_KEY = process.env.NEXT_PUBLIC_FORTMATIC_KEY
 export const PORTIS_ID = process.env.NEXT_PUBLIC_PORTIS_ID
-export const MUON_NODE_GATEWAY = process.env.NEXT_PUBLIC_MUON_NODE_GATEWAY
-export const INFO_URL = process.env.NEXT_PUBLIC_INFO_URL
+
+export const MUON_NODE_GATEWAY = 'https://node1.muon.net/v1/'
+export const INFO_URL = 'https://info.deus.finance'
 export const BRIDGE_URL = INFO_URL + '/bridge'
